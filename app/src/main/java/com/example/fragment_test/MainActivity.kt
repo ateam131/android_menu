bec9e38d797640403859da7a5c042bdddb1e66c0
ateam131 <37457478+ateam131@users.noreply.github.com>
2020-08-30 15:52:35 -0400

    Initial push

---
+++ app/src/main/java/com/example/fragment_test/MainActivity.kt
@@ -0,0 +1,48 @@
+package com.example.fragment_test
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val firstFragment = FirstFragment()
+        val secondFragment = SecondFragment()
+        val thirdFragment = ThirdFragment()
+
+        supportFragmentManager.beginTransaction().apply {
+            replace(R.id.flFragment, firstFragment)
+            commit()
+        }
+
+        btnFragment1.setOnClickListener {
+            supportFragmentManager.beginTransaction().apply {
+                replace(R.id.flFragment, firstFragment)
+                addToBackStack(null)
+                commit()
+            }
+        }
+
+        btnFragment2.setOnClickListener {
+            supportFragmentManager.beginTransaction().apply {
+                replace(R.id.flFragment, secondFragment)
+                addToBackStack(null)
+                commit()
+            }
+        }
+
+        btnFragment3.setOnClickListener {
+            supportFragmentManager.beginTransaction().apply {
+                replace(R.id.flFragment, thirdFragment)
+                addToBackStack(null)
+                commit()
+            }
+        }
+
+
+
+    }
+}
---
+++ app/src/main/java/com/example/fragment_test/ThirdFragment.kt
@@ -0,0 +1,11 @@
+package com.example.fragment_test
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+class ThirdFragment : Fragment(R.layout.fragment_third) {
+
+}
